2082a7bf5e88a60d7a1ac3aa1323f83023e8f6cf
Maryam Mohamed <165857090+maryamabdallahmohamed@users.noreply.github.com>
2024-11-24 23:55:41 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,24 +1,24 @@
 import unittest
-from AutomationTask import add
+from AutomationTask import addNumbers
 
 class TestAutomationTask(unittest.TestCase):
 
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(2, 3), 5)
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-2, -3), -5)
-    def test_add_positive_negative(self):
-        self.assertEqual(add(2, -3), -1)
-    def test_add_zero(self):
-        self.assertEqual(add(0, 5), 5)
-        self.assertEqual(add(5, 0), 5)
-    def test_add_float(self):
-        self.assertEqual(add(2.5, 3.5), 6.0)
-    def test_add_failure(self):
-        self.assertNotEqual(add(1, 2), 4)  
-    def test_add_non_numeric(self):
+    def test_addNumbers_positive_numbers(self):
+        self.assertEqual(addNumbers(2, 3), 5)
+    def test_addNumbers_negative_numbers(self):
+        self.assertEqual(addNumbers(-2, -3), -5)
+    def test_addNumbers_positive_negative(self):
+        self.assertEqual(addNumbers(2, -3), -1)
+    def test_addNumbers_zero(self):
+        self.assertEqual(addNumbers(0, 5), 5)
+        self.assertEqual(addNumbers(5, 0), 5)
+    def test_addNumbers_float(self):
+        self.assertEqual(addNumbers(2.5, 3.5), 6.0)
+    def test_addNumbers_failure(self):
+        self.assertNotEqual(addNumbers(1, 2), 4)  
+    def test_addNumbers_non_numeric(self):
         with self.assertRaises(TypeError):
-            add("Hello", 3)  
+            addNumbers("Hello", 3)  
 
 if __name__ == '__main__':
     unittest.main()
